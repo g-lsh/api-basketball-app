df507dcc8198440a107634f061b74f48f5ee9744
Ghislain H <telitos@gmail.com>
2016-12-15 23:37:27 +0000

    updated migrations

--- db/migrations/20161215200002_create_teams.js
+++ db/migrations/20161215200002_create_teams.js
@@ -3,12 +3,12 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('teams', function (table) {
     table.increments('id');
     table.string('name')
+    table.string('nba_code')
     table.string('location')
     table.string('logo')
     table.string('background')
     table.boolean('division')
     table.string('website')
-    // What do I write for the API, and Twitter API
   });
 };
 
