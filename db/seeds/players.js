de89f6dfc2b88c4ed8ad9b3d1b1400d3c5912c73
Ghislain H <telitos@gmail.com>
2016-12-16 01:36:37 +0000

    adds one player seed

--- db/seeds/players.js
+++ db/seeds/players.js
@@ -1,9 +1,17 @@
 exports.seed = function(knex, Promise) {
-  return knex('users').del()
+  return knex('players').del()
     .then(function () {
-      return Promise.all([
-        knex('players').insert({email: 'alice@test.com', password: "alice"}),
-        knex('players').insert({email: 'bob@test.com', password: 'bob'}),
+      return Promise.all ([
+        knex('players').insert ({
+          first_name: "Dwight",
+          last_name: "Howard",
+          team_id: 1,
+          custom_team_id: 1,
+          head_shot: "http://a3.espncdn.com/combiner/i?img=%2Fi%2Fheadshots%2Fnba%2Fplayers%2Ffull%2F2384.png",
+          college: "SW Atlanta Christian Academy (GA)/USA",
+          height: 211,
+          weight: 250,
+        }),
       ]);
     });
   };
