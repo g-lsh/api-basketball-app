939b94cc2d353f49e7eaf23f182537fde82a89f6
Ghislain H <telitos@gmail.com>
2016-12-22 02:00:19 +0000

    minor changes since last commit (removed debugger in seed players.js

--- db/seeds/seed_players.js
+++ db/seeds/seed_players.js
@@ -2,15 +2,15 @@ const api = require('../../nbaApi/nbaApi.js')
 
 const mapperFunction = (team_id, playersJSON) => {
   let collection = playersJSON.map((playerJSON) => {
-  return {
-    api_id: playerJSON.id,
-    team_id: team_id,
-    first_name: playerJSON.first_name,
-    last_name: playerJSON.last_name,
-    birthdate: playerJSON.birthdate,
-    position: playerJSON.position
-  }
-})
+    return {
+      api_id: playerJSON.id,
+      team_id: team_id,
+      first_name: playerJSON.first_name,
+      last_name: playerJSON.last_name,
+      birthdate: playerJSON.birthdate,
+      position: playerJSON.position
+    }
+  })
   return collection
 }
 
@@ -18,19 +18,20 @@ const insertPlayers = (knex, playersJSON) => {
 
   knex('teams')
     .select('id')
+    //The next statement is where the flaw is, we don't want one id, we want
+    //each ones for each player. Otherwise we can just pass the team_id trough
+    //insertPlayers as an argument
     .where('api_id', playersJSON[0].team_id)
     .then((idArray) => {
       let team_id = idArray[0].id
       const collection = mapperFunction(team_id, playersJSON)
-      debugger
       // execute and return the knex query
       return knex('players')
         .insert(collection)
       }).then((data) => {
-        debugger
         console.log('Players inserted into database')
       });
-    }
+}
 
 
 module.exports = function(knex) {
