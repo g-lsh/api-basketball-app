c5c5f32ffea7ddd391d000f0bf1d60bba5a71945
Ghislain H <telitos@gmail.com>
2017-01-01 23:10:58 +0000

    adds signup feature

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,52 @@
 
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require('bcrypt');
 
 module.exports = (knex) => {
 
-  router.get("/", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      .then((results) => {
-        res.json(results);
-    });
+  router.post("/signup", (req, res) => {
+    if (!req.body) {
+      console.log("No body in the request");
+      res.status(400).send("The body of the request is empty");
+    }
+
+    let {email, password} = req.body;
+
+    const checkEmail  = (cb) => {
+      let existingEmail = "";
+      knex('users')
+        .select('id', 'email')
+        .where('email', email)
+        .then((userArray) => {
+          if (userArray[0]) {
+            existingEmail = userArray[0].email;
+          }
+          if (!existingEmail) {
+            cb();
+          } else {
+            res.status(400).send(`The email adress: ${existingEmail} already exists.`);
+          }
+        });
+    }
+
+    const insertNewUser = () => {
+      knex('users')
+        .insert({
+         email: email,
+         password: bcrypt.hashSync(password, 10)
+       }, 'id')
+        .then((arrayOfId) => {
+          let user_id = arrayOfId[0];
+          res.json(user_id);
+        })
+        .catch((err) => {
+          res.status(400);
+        });
+    }
+
+    checkEmail(insertNewUser);
+
   });
 
   return router;
