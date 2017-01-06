8b0dd2a1e7c07dc13e4f3e3f92ffab79f11e0824
Ghislain H <telitos@gmail.com>
2017-01-06 23:19:13 +0000

    modifies custom_teams/new post to check for name and user before inserting

--- routes/custom_teams.js
+++ routes/custom_teams.js
@@ -26,13 +26,13 @@ module.exports = (knex) => {
     } else {
       const {custom_team_id} = req.params;
       knex('custom_team_id_to_player_id')
-      .select('player_id')
-      .where('custom_team_id', custom_team_id)
-      .then((results) => {
-        res.json(results);
-      }).catch((err) => {
-        res.status(500).json({error: `something went wrong when fetching custom team ${custom_team_id} players`});
-      })
+        .select('player_id')
+        .where('custom_team_id', custom_team_id)
+        .then((results) => {
+          res.json(results);
+        }).catch((err) => {
+          res.status(500).json({error: `something went wrong when fetching custom team ${custom_team_id} players`});
+        })
     }
   })
 
@@ -43,21 +43,32 @@ module.exports = (knex) => {
     } else {
         const user = req.currentUser;
         const {name, players} = req.body;
-        knex('custom_teams')
-          .insert({
-          name: name,
-          user_id: user.id
-        }, 'id')
-      .then((arrayOfId) => {
-        const custom_team_id = arrayOfId[0];
-        const collection = JSON.parse(players).map((player_id) => {
-          return {'player_id': player_id, 'custom_team_id': custom_team_id};
-        })
-        return knex('custom_team_id_to_player_id')
-        .insert(collection)
-      }).then((data) => {
-        console.log('Players associated with custom team.');
-      });
+        const newCustomTeam = {name, user_id: user.id}
+        knex('custom_teams').select('id')
+          .where(newCustomTeam)
+          .then((arrayOfId) => {
+            if (!arrayOfId.length) {
+              knex('custom_teams')
+                .insert(newCustomTeam, ['id', 'name'])
+                .then((customTeamArray) => {
+                  const customTeam = customTeamArray[0];
+                  res.json(customTeam);
+                  const collection = JSON.parse(players).map((player_id) => {
+                    return {'player_id': player_id, 'custom_team_id': customTeam.id};
+                  })
+                  return knex('custom_team_id_to_player_id')
+                  .insert(collection)
+                })
+                .then((data) => {
+                  console.log('Players associated with custom team.');
+                })
+                .catch((err) => {
+                  res.status(500).json({ error: "Something went wrong when trying to add new custom team."});
+                })
+            } else {
+                res.status(401).json({ error: "A custom team with this name already exists for this user."});
+            }
+          })
     }
   })
 
@@ -73,8 +84,8 @@ module.exports = (knex) => {
         .then((data) => {
           if (data.length) {
             knex('custom_team_id_to_player_id').where({
-              'custom_team_id': custom_team_id,
-              'player_id': player_id
+              custom_team_id: custom_team_id,
+              player_id: player_id
               })
             .del()
             .then((data) => {
@@ -103,8 +114,8 @@ module.exports = (knex) => {
         const {custom_team_id} = req.params;
         knex('custom_teams').select('id')
           .where({
-            'user_id': user.id,
-            'id': custom_team_id
+            user_id: user.id,
+            id: custom_team_id
           })
           .del()
           .then((data) => {
