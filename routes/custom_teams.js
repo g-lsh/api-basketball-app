0cc1168db9ce3bf78fa2dbbcabb0046649021d14
Ghislain H <telitos@gmail.com>
2017-01-07 18:52:59 +0000

    removes JSON.parse in custom_teams/new route

--- routes/custom_teams.js
+++ routes/custom_teams.js
@@ -7,7 +7,6 @@ const authenticate = require('../authenticate/authenticate.js');
 module.exports = (knex) => {
 /*To refractor: use authenticate once on all routes.*/
 
-
   /*Route to fetch custom team for a given user.*/
   router.get("/", authenticate, (req, res) => {
     if (!req.currentUser) {
@@ -17,8 +16,8 @@ module.exports = (knex) => {
         const user = req.currentUser;
         knex('custom_teams').where('user_id', user.id)
         .then((results) => {
-        res.json(results);
-      })
+          res.json(results);
+        })
     }
   })
 
@@ -34,9 +33,10 @@ module.exports = (knex) => {
         .where('custom_team_id', custom_team_id)
         .then((results) => {
           res.json(results);
-        }).catch((err) => {
-          res.status(500).json({error: `something went wrong when fetching custom team ${custom_team_id} players`});
         })
+        .catch((err) => {
+          res.status(500).json({error: `something went wrong when fetching custom team ${custom_team_id} players`});
+        });
     }
   })
 
@@ -58,7 +58,7 @@ module.exports = (knex) => {
             .then((customTeamArray) => {
               const customTeam = customTeamArray[0];
               res.json(customTeam);
-              const collection = JSON.parse(players).map((player_id) => {
+              const collection = players.map((player_id) => {
                 return {'player_id': player_id, 'custom_team_id': customTeam.id};
               })
               return knex('custom_team_id_to_player_id')
